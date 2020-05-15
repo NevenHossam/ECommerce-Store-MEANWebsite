d5ea0047fbf0825321bb0fc205bf8051a9aa5ab7
AmmarIbrahem <ammarslm1994@gmail.com>
2020-05-15 03:28:46 +0200

    small fixes

--- server.js
+++ server.js
@@ -4,9 +4,9 @@ const app = express();
 
 app.use(express.static(__dirname + '/dist/ECommerceAngular'));
 
-app.get('*', function(req, res){
+app.get('/*', function(req, res){
     res.sendFile(path.join(__dirname+ '/dist/ECommerceAngular/index.html'));
 });
 
-app.listen(process.env.PORT || 8080);   
+app.listen(process.env.PORT || 8888);   
 
