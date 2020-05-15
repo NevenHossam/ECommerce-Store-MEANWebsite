66e5e0e352de4a283213a272691005900d2151b6
AmmarIbrahem <ammarslm1994@gmail.com>
2020-05-15 02:21:49 +0200

    first committ

--- server.js
+++ server.js
@@ -8,5 +8,5 @@ app.get('*', function(req, res){
     res.sendFile(path.join(__dirname+ '/dist/ECommerceAngular/index.html'));
 });
 
-app.listen(process.env.PORT || 8080);
+app.listen(process.env.PORT || 8080);   
 
