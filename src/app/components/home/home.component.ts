dc4e605022e076d7e52df49ef5ec4f64ec2945ba
NevenHossam <NevenHossam50@gmail.com>
2020-05-15 20:05:43 +0200

    minor change

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -16,25 +16,7 @@ export class HomeComponent implements OnInit {
     private userService: UsersService
   ) {
     this.getPromotedProduts();
-    // debugger;
-    this.prdService.initLocalStorageName();
-    this.prdService.shoppingCartListOfProducts = this.prdService.getShoppingCartContent();
-    this.prdService.shoppingCartTotalCost = this.prdService.getTotalCostOfCart();
-    // debugger;
-    userService.currentUserInfo = this.userService.getCurrentUser();
-    // debugger;
-    if (
-      this.prdService.shoppingCartListOfProducts == null ||
-      this.prdService.shoppingCartListOfProducts.length == 0
-    )
-      this.prdService.shoppingCartListOfProducts = [];
-    else
-      this.prdService.shoppingCartListOfProducts.forEach((product) => {
-        if (product.promotion)
-          product.totalCost = product.price - product.promotion;
-        else product.totalCost = product.price;
-      });
-      // debugger;
+   
     }
 
 
--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -3,6 +3,7 @@ import { UsersService } from 'src/app/services/users.service';
 import { LoginModel } from 'src/app/models/LoginModel';
 import { Router } from '@angular/router';
 import * as jwtDecoder from 'jwt-decode';
+import { ProductsService } from 'src/app/services/products.service';
 
 @Component({
   selector: 'app-login',
@@ -17,7 +18,7 @@ export class LoginComponent implements OnInit {
   invalidLogin: boolean = false;
   currentUser;
 
-  constructor(private userServices: UsersService, private router: Router) {}
+  constructor(private userServices: UsersService, private router: Router, private prdService: ProductsService) {}
 
   ngOnInit(): void {}
 
@@ -36,6 +37,21 @@ export class LoginComponent implements OnInit {
             'currentuser',
             JSON.stringify(this.userServices.getCurrentUser())
           );
+          this.prdService.initLocalStorageName();
+          this.prdService.shoppingCartListOfProducts = this.prdService.getShoppingCartContent();
+          this.prdService.shoppingCartTotalCost = this.prdService.getTotalCostOfCart();
+          this.userServices.currentUserInfo = this.userServices.getCurrentUser();
+          if (
+            this.prdService.shoppingCartListOfProducts == null ||
+            this.prdService.shoppingCartListOfProducts.length == 0
+          )
+            this.prdService.shoppingCartListOfProducts = [];
+          else
+            this.prdService.shoppingCartListOfProducts.forEach((product) => {
+              if (product.promotion)
+                product.totalCost = product.price - product.promotion;
+              else product.totalCost = product.price;
+            });
           this.invalidLogin = false;
           this.router.navigate(['home']);
         },
